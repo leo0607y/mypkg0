1d0d3b7a83d1ba1bab231be04a7b66fb2d3c4a05
leo0607y <reochimaru@gmail.com>
2025-12-29 15:12:21 +0900

    setup修正

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 import os
 from glob import glob
 
@@ -6,12 +6,13 @@ package_name = 'my_cpu_monitor'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Launchファイルをインストール対象に追加
         (os.path.join('share', package_name), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
@@ -20,14 +21,10 @@ setup(
     maintainer_email='reochimaru@gmail.com',
     description='robosys2025_2',
     license='BSD-3-Clause',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'monitor = my_cpu_monitor.monitor_node:main'
+            'monitor = my_cpu_monitor.monitor_node:main',
             'listener = my_cpu_monitor.listener_node:main',
         ],
     },
